32df95a04f0c028121fd38787f5a712c960d18c5
anagomesmat <90735968+anagomesmat@users.noreply.github.com>
2021-10-03 21:02:12 -0300

    adcionar os arquivos

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+//Desafios desta aula!
+//Terminar todo o design que está no figma (disponível abaixo)
+//Adicionar suas redes sociais de forma que quem clique consiga abrí-las
